66c725ab3bd33f362598ee5f385f847f7cbed1f8
merrill-dc5 <51773353+merrill-dc5@users.noreply.github.com>
2022-08-29 14:19:17 +0900

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello TAPTAPTAP!\n');
+  res.send('Hello TAPTAPTAP making!\n');
 });
 
 app.listen(PORT, HOST);
